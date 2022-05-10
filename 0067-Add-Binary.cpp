4625e50d58639a9d3dfcfadf04c0fb5b42f25fe5
Yun-Hsuan <46278823+yxuan0329@users.noreply.github.com>
2022-05-10 22:53:59 +0800

    Create 0067-Add-Binary.cpp

---
+++ 0067-Add-Binary.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string res;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        while(i >= 0 || j >= 0){ // binary add
+            int sum = carry;
+            if(i >= 0) sum += a[i--] - '0';
+            if(j >= 0) sum += b[j--] - '0';
+            carry = sum > 1 ? 1 : 0;
+            res += to_string(sum % 2);
+        }
+        if(carry) res += to_string(carry); // most significant digit
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
