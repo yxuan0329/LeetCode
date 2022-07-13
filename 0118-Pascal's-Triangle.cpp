7207581bc1a25d3cd036a6bd47b05eb2b549cdf9
Yun-Hsuan <46278823+yxuan0329@users.noreply.github.com>
2022-07-13 16:41:17 +0800

    Create 0118-Pascal's-Triangle.cpp

---
+++ 0118-Pascal's-Triangle.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> ans(n);             // initialize n rows
+        for(int i = 0; i < n; i++) {
+            ans[i] = vector<int>(i+1,1);        // ith row(0-indexed) has i+1 elements
+            for(int j = 1; j < i; j++)          // 1st and last elements will be 1, rest will be sum of two elements from above row
+                ans[i][j] = ans[i - 1][j] + ans[i - 1][j - 1];            
+        }
+        return ans;
+    }
+};
